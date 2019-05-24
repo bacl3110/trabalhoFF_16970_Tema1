a186debedebf08edf040b0bc5a915c32ff916e11
Bruno Lopes <brunolopes3000@gmail.com>
2019-05-24 16:12:52 +0100

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+*  -------------------------------------------------
+* <copyright file=" Program " company="IPCA"/>
+* <date> 5/24/2019 4:12:47 PM </date>
+* <author> bruno </author>
+* <email> a16970@alunos.ipca.pt </email>
+* <desc>
+*   
+* </desc>
+* -------------------------------------------------
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
